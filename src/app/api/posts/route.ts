3e6afb1cd2de6f3e55e595dd0ef11abe33354e0e
Daisuke <d.suke.kida@gmail.com>
2025-07-08 17:17:03 +0900

    fix: edge runtimeで実行されるように修正

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,6 +1,8 @@
 import { getAllPosts } from '@/app/lib/blog';
 import { NextResponse } from 'next/server';
 
+export const runtime = 'edge'
+
 export async function GET() {
   const posts = getAllPosts();
   return NextResponse.json(posts);
